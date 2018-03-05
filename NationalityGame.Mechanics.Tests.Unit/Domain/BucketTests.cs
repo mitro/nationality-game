8cc296615ace28d0a03864ecab738c2f85ecdddb
Dmitriy Melnik <d.b.melnik@yandex.ru>
2018-03-06 01:11:16 +0600

    Add Bucket tests

---
+++ NationalityGame.Mechanics.Tests.Unit/Domain/BucketTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using FluentAssertions;
+using NationalityGame.Mechanics.Domain;
+using Xunit;
+
+namespace NationalityGame.Mechanics.Tests.Unit.Domain
+{
+    public class BucketTests
+    {
+        [Fact]
+        public void Should_set_properties()
+        {
+            var bucket = new Bucket("Thai", new Point(1, 2));
+
+            bucket.Nationality.Should().Be("Thai");
+            bucket.Center.Should().Be(new Point(1, 2));
+        }
+
+        [Fact]
+        public void Should_match_photo_if_nationalities_equal()
+        {
+            var bucket = new Bucket("Thai", new Point(1, 2));
+
+            bucket.Matches(new Photo(new Point(), "Thai", string.Empty)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Should_match_photo_if_nationalities_equal_but_different_cases()
+        {
+            var bucket = new Bucket("Thai", new Point(1, 2));
+
+            bucket.Matches(new Photo(new Point(), "thai", string.Empty)).Should().BeTrue();
+        }
+
+        [Fact]
+        public void Should_not_match_photo_if_nationalities_not_equal()
+        {
+            var bucket = new Bucket("Thai", new Point(1, 2));
+
+            bucket.Matches(new Photo(new Point(), "Chinese", string.Empty)).Should().BeFalse();
+        }
+    }
+}
